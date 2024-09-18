16d7c0b1201769c99361333dac5382ccc50c4e3d
Prof. Luiz Augusto Rodrigues <prof.luiz.a.rodrigues.2018@gmail.com>
2024-09-17 20:34:01 -0400

    Atualização Encontro 07

--- Turma_A/codigo/Projeto_Estoque/ProjetoEstoque/src/App.java
+++ Turma_A/codigo/Projeto_Estoque/ProjetoEstoque/src/App.java
@@ -1,9 +1,47 @@
+import dominio.ClasseProduto;
 import visao.ClasseProdutoVisao;
+import java.time.LocalDate;
 
 public class App {
     public static void main(String[] args) throws Exception {
         ClasseProdutoVisao visao = new ClasseProdutoVisao();
-        //visao.Exibir();
-        visao.Criar();
+
+        // Imprimir tudo.
+        System.out.println("Imprimindo tudo.");
+        visao.ExibirPorLinha();
+
+        // Criar nova instância.
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Criando uma nova instância.");
+        ClasseProduto novo = new ClasseProduto(0, "Teste", LocalDate.now());
+        visao.Criar(novo);
+        visao.ImprimirPorLinha(novo);
+
+
+        // Imprimir tudo.
+        //visao.ExibirPorLinha();
+
+        // Editar instância recém adicionada.
+        System.out.println("--------------------------------------------------------");        
+        System.out.println("Alterando uma instância.");
+        ClasseProduto alterado = new ClasseProduto();
+        alterado.setCodigo(0);
+        alterado.setDescricao("Teste de alteração");
+        alterado.setDataInclusao(LocalDate.now());
+        visao.Editar(6, alterado);
+        visao.ImprimirPorLinha(6);
+
+        // Imprimir tudo.
+        System.out.println("Imprimindo tudo.");
+        visao.ExibirPorLinha();
+
+        // Remover o item 6.
+        System.out.println("Removendo o item 6.");
+        visao.Remover(6);
+
+        // Imprimir tudo.
+        System.out.println("--------------------------------------------------------");        
+        System.out.println("Imprimindo tudo.");
+        visao.ExibirPorLinha();        
     }
 }
--- Turma_A/codigo/Projeto_Estoque/ProjetoEstoque/src/visao/ClasseProdutoVisao.java
+++ Turma_A/codigo/Projeto_Estoque/ProjetoEstoque/src/visao/ClasseProdutoVisao.java
@@ -5,23 +5,31 @@ import java.time.LocalDate;
 import dominio.ClasseProduto;
 import servico.ClasseProdutoServico;
 
-
-
 public class ClasseProdutoVisao {
     
+    private ClasseProdutoServico srv;
+
     public ClasseProdutoVisao(){
+        this.srv = new ClasseProdutoServico();
     }
 
     public void Exibir(){
-        ClasseProdutoServico srv = new ClasseProdutoServico();
-        ArrayList<ClasseProduto> lista = srv.Navegar();
+        ArrayList<ClasseProduto> lista = this.srv.Navegar();
         System.out.println("===========================================================");
         for (ClasseProduto cp : lista) {
             this.Imprimir(cp);
         }
     }
 
-    private void Imprimir(ClasseProduto cp){
+    public void ExibirPorLinha(){
+        ArrayList<ClasseProduto> lista = this.srv.Navegar();
+        System.out.println("===========================================================");
+        for (ClasseProduto cp : lista) {
+            this.ImprimirPorLinha(cp);
+        }
+    }
+
+    public void Imprimir(ClasseProduto cp){
         System.out.println("Classe de Produto:");
         System.out.println("Código: " + cp.getCodigo());
         System.out.println("Descrição: " + cp.getDescricao());
@@ -29,10 +37,43 @@ public class ClasseProdutoVisao {
         System.out.println("--------------------------------------------------------");
     }
 
-    public void Criar(){
-        ClasseProduto novo = new ClasseProduto(0, "Teste", LocalDate.now());
-        ClasseProdutoServico srv = new ClasseProdutoServico();
-        srv.Adicionar(novo);
-        Imprimir(novo);
+    public void ImprimirPorLinha(ClasseProduto cp){
+        String mensagem = "";
+        mensagem += "Classe de Produto: ";
+        mensagem += "Código: " + cp.getCodigo() + " | ";
+        mensagem += "Descrição: " + cp.getDescricao() + " | ";
+        mensagem += "Data de Inclusão: " + cp.getDataInclusao();
+        System.out.println(mensagem);
+    }
+
+    public void ImprimirPorLinha(int chave){
+        ClasseProduto cp = this.srv.Ler(chave);
+        this.ImprimirPorLinha(cp);
+    }
+
+
+    public void Criar(ClasseProduto novo){
+        this.srv.Adicionar(novo);
+    }
+
+    public void Editar(int chave, ClasseProduto alt){
+        ClasseProduto cp = this.srv.Ler(chave);
+        if (cp != null){
+            cp.setDescricao(alt.getDescricao());
+        }
+        else{
+            System.out.println("Item não localizado.");
+        }
+    }
+
+    public void Remover(int chave){
+        ClasseProduto cp = this.srv.Ler(chave);
+        if (cp != null){
+            this.srv.Remover(chave);
+        }
+        else{
+            System.out.println("Item não localizado.");
+        }
+
     }
 }
